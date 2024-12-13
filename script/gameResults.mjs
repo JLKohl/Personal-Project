ec804d6a6055ecb6069ed58774f2850df4622f3e
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-12-13 11:48:06 -0500

    finished up project goals :)
    # Conflicts:
    #	game.html
    #	styles/main_styles.css

--- script/gameResults.mjs
+++ script/gameResults.mjs
@@ -7,7 +7,9 @@ export function gameResults(results, pokemon1 = null, pokemon2=null) {
     
     switch (results) {
         case "winner":
-            message.textContent = "Winner!!";
+            message.innerHTML = `Congratulations!! You have won both pokemon and they will be added to your Pokédeck!
+                <a href='pokedeck.html'>click here</a> to see your Pokédeck, or close the tab, the game will reload and you can win some more!`;
+            
             savePokemon(pokemon1, pokemon2);
             break;
         case "loser":
